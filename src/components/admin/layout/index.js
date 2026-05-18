d7db9006d921e0b82b1b51fd05bd97a3f2023ab7
Rishwi Jagesar <rishwijagesar@hotmail.com>
2026-05-18 17:56:59 +0200

    feat(admin): export TabContent layout component

--- src/components/admin/layout/index.js
+++ src/components/admin/layout/index.js
@@ -2,3 +2,4 @@ export { default as AdminHeader } from './AdminHeader';
 export { default as AdminPageContainer } from './AdminPageContainer';
 export { default as AdminTabs } from './AdminTabs';
 export { default as AdminWorkspace } from './AdminWorkspace';
+export { default as TabContent } from './TabContent';
