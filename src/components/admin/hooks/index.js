d1bcabf52bfe7db057034790f311312419540f04
Rishwi Jagesar <rishwijagesar@hotmail.com>
2026-05-18 17:32:38 +0200

    feat(admin): add hook barrel exports

---
+++ src/components/admin/hooks/index.js
@@ -0,0 +1,4 @@
+export { default as useApiConfig } from './useApiConfig';
+export { default as useAsyncAction } from './useAsyncAction';
+export { default as useCustomerForm } from './useCustomerForm';
+export { default as useDeploymentPolling } from './useDeploymentPolling';
