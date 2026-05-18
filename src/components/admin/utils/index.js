ea677fe9a18aaad7f645f2f02912454ce8e34a8e
Rishwi Jagesar <rishwijagesar@hotmail.com>
2026-05-18 17:40:17 +0200

    feat(admin): export copyToClipboard utility

--- src/components/admin/utils/index.js
+++ src/components/admin/utils/index.js
@@ -1,3 +1,4 @@
+export { default as copyToClipboard } from './copyToClipboard';
 export { default as formatCurrency } from './formatCurrency';
 export { default as formatDate } from './formatDate';
 export { default as formatStatus } from './formatStatus';
