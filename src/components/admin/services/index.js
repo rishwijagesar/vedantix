3f1a24723482d6aa8b411441693886f08e777477
Rishwi Jagesar <rishwijagesar@hotmail.com>
2026-05-18 17:43:49 +0200

    feat(admin): add services barrel exports

---
+++ src/components/admin/services/index.js
@@ -0,0 +1,6 @@
+export * from './customerService';
+export * from './deploymentService';
+export * from './invoiceService';
+export * from './leadService';
+export * from './mailboxService';
+export * from './http';
