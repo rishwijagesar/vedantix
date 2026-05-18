8ae45642bc06eb65807b340c9538c30d08c6ee72
Rishwi Jagesar <rishwijagesar@hotmail.com>
2026-05-18 17:51:03 +0200

    feat(admin): export layout components from admin index

--- src/components/admin/index.js
+++ src/components/admin/index.js
@@ -1,6 +1,7 @@
 export * from './config';
 export * from './constants';
 export * from './hooks';
+export * from './layout';
 export * from './sections';
 export * from './services';
 export * from './utils';
