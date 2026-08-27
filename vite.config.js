e85c333658eeeff5da1a3dbe223e34cab61d9af9
Rishwi Jagesar <rishwijagesar@hotmail.com>
2026-08-28 01:18:01 +0200

    refactor: remove Base44 Vite plugin from migration branch

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
-import base44 from "@base44/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   logLevel: "error",
-  plugins: [
-    base44({
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === "true",
-      hmrNotifier: false,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: false,
-    }),
-    react(),
-  ],
+  plugins: [react()],
   server: {
     proxy: {
       "/provisioning-api": {
