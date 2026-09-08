7aad8027a1d598b9903c1babcdef78897117f793
Rishwi Jagesar <rishwijagesar@hotmail.com>
2026-09-08 12:57:09 +0200

    Fix Button prop type inference

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -34,15 +34,29 @@ const buttonVariants = cva(
   }
 )
 
-const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
-  const Comp = asChild ? Slot : "button"
-  return (
-    (<Comp
-      className={cn(buttonVariants({ variant, size, className }))}
-      ref={ref}
-      {...props} />)
-  );
-})
+/**
+ * @typedef {React.ComponentPropsWithoutRef<"button"> &
+ *   import("class-variance-authority").VariantProps<typeof buttonVariants> & {
+ *     asChild?: boolean
+ *   }} ButtonProps
+ */
+
+const Button = React.forwardRef(
+  /**
+   * @param {ButtonProps} props
+   * @param {React.ForwardedRef<HTMLButtonElement>} ref
+   */
+  function Button({ className, variant, size, asChild = false, ...props }, ref) {
+    const Comp = asChild ? Slot : "button"
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
